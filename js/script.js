44ac1935dd64a492bf362eb6718218364febed55
Satishchoudhary94 <148007944+Satishchoudhary94@users.noreply.github.com>
2024-08-16 21:45:35 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 'use strict';
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+    if (slideIndex < 1) {slideIndex = slides.length}
+    if (slideIndex > slides.length) {slideIndex = 1}
+    showSlides();
+}
+
 
 // navbar variables
 const navbarNav = document.querySelector('.navbar-nav');
 const navbarToggleBtn = document.querySelector('.nav-toggle-btn');
+
 
 // navbar toggle functionality
 navbarToggleBtn.addEventListener('click', function () {
