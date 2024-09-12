68d34660974f0288a5643c796a1b912e0409747e
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2024-09-12 22:02:58 +0530

    donr

--- event.js
+++ event.js
@@ -1,22 +1,32 @@
 document.getElementById('event-registration-form').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent default form submission
+    e.preventDefault(); 
 
-    // Collect form data
-    const formData = new FormData(this);
-    console.log(this.action);
-    // Send data to the Google Apps Script via fetch
-    fetch(this.action, {
-        method: this.method,
-        body: formData
+    console.log(this.name.value);
+
+    fetch("https://cb-kare-server.onrender.com/event/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            fullName: this.name.value,
+            email: this.email.value,
+            phone: this.phone.value,
+            event: this.event.value,
+            registerNumber: this.reg_no.value,
+            department: this.department.value,
+            event:this.event.value
+        })
     })
     .then(response => response.text())
     .then(data => {
         // Show success message
+        console.log(data);
         document.getElementById('success-message').style.display = 'block';
         this.reset(); // Optionally reset the form after submission
     })
     .catch(error => {
         console.error('Error:', error);
-        alert('There was an error submitting the form. Please try again.'); // Optionally show error message
+        alert('There was an error submitting the form. Please try again.'); 
     });
 });
